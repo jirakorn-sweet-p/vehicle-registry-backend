003201de20ef120e2c99f626f5a1cd15a050a06f
jirakorn-sweet-p <jirakorn@kkumail.com>
2026-08-11 19:37:21 +0700

    config

--- frontend/assets/config.js
+++ frontend/assets/config.js
@@ -1,4 +1,4 @@
 // config.js
 // แก้ไข API_BASE_URL ให้ตรงกับ URL ของ backend ที่ deploy บน Render.com
 // เช่น "https://your-backend-service.onrender.com"
-window.API_BASE_URL = window.API_BASE_URL || "https://vehicle-registry-frontend.onrender.com/";
+window.API_BASE_URL = window.API_BASE_URL || "https://vehicle-registry-backend.onrender.com";
